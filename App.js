e4a261488263535ce213cde225adbf6cceb48f24
문성현 <shine7329@gmail.com>
2023-02-13 21:22:08 +0900

    feat: async-storage 저장 기능 추가

--- App.js
+++ App.js
@@ -1,23 +1,50 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { View, StyleSheet } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { colors } from './colors';
 import { StatusBar } from 'expo-status-bar';
 import TodoList from './components/TodoList';
 import TodoInput from './components/TodoInput';
 
+const STORAGE_KEY = '@todos';
+
 const App = () => {
   const [todos, setTodos] = useState({});
 
-  const addTodo = (text) => {
-    const newTodo = { [Date.now()]: { text, completed: false } };
-    setTodos({ ...todos, ...newTodo });
+  const loadTodos = async () => {
+    try {
+      const result = await AsyncStorage.getItem(STORAGE_KEY);
+      setTodos(result ? JSON.parse(result) : {});
+    } catch (e) {
+      console.log(e);
+    }
   };
 
-  const deleteTodo = (id) => {
+  const saveTodos = async (value) => {
+    try {
+      await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(value));
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const addTodo = async (text) => {
+    const newTodos = { ...todos };
+    newTodos[Date.now()] = { text, completed: false };
+    setTodos(newTodos);
+    await saveTodos(newTodos);
+  };
+
+  const deleteTodo = async (id) => {
     const newTodos = { ...todos };
     delete newTodos[id];
     setTodos(newTodos);
+    await saveTodos(newTodos);
   };
+
+  useEffect(() => {
+    loadTodos();
+  }, []);
 
   return (
     <View style={styles.container}>
