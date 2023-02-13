76c6ebbecaf204fa9577679d4fb92f78b6a23c37
문성현 <shine7329@gmail.com>
2023-02-13 21:00:43 +0900

    feat: Todo 레이아웃 조합

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
+import { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { colors } from './colors';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import TodoList from './components/TodoList';
+import TodoInput from './components/TodoInput';
 
-export default function App() {
+const App = () => {
+  const [todos, setTodos] = useState({});
+
+  const addTodo = (text) => {
+    const newTodo = { [Date.now()]: { text, completed: false } };
+    setTodos({ ...todos, ...newTodo });
+  };
+
+  const deleteTodo = (id) => {
+    const newTodos = { ...todos };
+    delete newTodos[id];
+    setTodos(newTodos);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='light' />
+      <TodoInput addTodo={addTodo} />
+      <TodoList todos={todos} deleteTodo={deleteTodo} />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: colors.bg,
   },
 });
+
+export default App;
